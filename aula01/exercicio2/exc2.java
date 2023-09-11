8d12b2dd0b6274c6e6ca28a0054a5e088a357ca3
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-09-11 15:59:12 -0300

    Update exc2.java
    Exercicio resolvido.

--- aula01/exercicio2/exc2.java
+++ aula01/exercicio2/exc2.java
@@ -1,1 +1,15 @@
 // Crie um programa que receba 2 números e imprima na tela.
+import java.util.Scanner;
+public class exc2 {
+    public static void main(String[] args){
+
+        Scanner recebeNmr = new Scanner(System.in);
+        System.out.println("insira o primeiro numero :");
+        int nmr1 = recebeNmr.nextInt();
+        System.out.println("insira o segundo numero:");
+        int nmr2 = recebeNmr.nextInt();
+        recebeNmr.close();
+        System.out.println("Os numero inseridos foram: " + nmr1 + " e " + nmr2);
+
+    }
+}
