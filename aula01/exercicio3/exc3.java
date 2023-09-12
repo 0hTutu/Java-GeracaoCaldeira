17c996d824b41309543c4e5d679381ad799f3772
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-09-11 23:24:44 -0300

    Update exc3.java
    Exercicio finalizado.

--- aula01/exercicio3/exc3.java
+++ aula01/exercicio3/exc3.java
@@ -1,1 +1,24 @@
 //Crie um programa que leia 3 números, imprima o maior, o menor e a média aritmética.
+import java.util.Scanner;
+
+public class exc3 {
+    public static void  main(String[] args){
+
+        Scanner nmr = new Scanner(System.in);
+        System.out.println("Digite 3 numeros.");
+        System.out.println("Primeiro numero:");
+            int n1 = nmr.nextInt();
+        System.out.println("Segundo numero");
+            int n2 = nmr.nextInt();
+        System.out.println("Terceiro numero");
+            int n3 = nmr.nextInt();
+
+        int maiorN = Integer.max(Integer.max(n1, n2), n3);
+        int menorN = Integer.min(Integer.min(n1, n2), n3);
+        double media = (n1 + n2 + n3) / 3;
+
+        System.out.println("Concluido!");
+        System.out.println("Maior numero: " + maiorN + "." + " Menor numero: " + menorN + ". " + "A média aritmética: " + media + ".");
+
+    }
+}
