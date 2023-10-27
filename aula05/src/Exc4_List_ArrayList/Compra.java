ba0c683912dfefe23b5c6416ae457867808d2301
Arthur <arthurmullercavalheiro@gmail.com>
2023-10-27 16:37:43 -0300

    Métodos get-set criados.

--- aula05/src/Exc4_List_ArrayList/Compra.java
+++ aula05/src/Exc4_List_ArrayList/Compra.java
@@ -1,7 +1,26 @@
 package src.Exc4_List_ArrayList;
 
+import java.text.SimpleDateFormat;
+
 public class Compra {
     String nome;
     String dataDeValidade;
 
+    private static SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataDeValidade() {
+        return dataDeValidade;
+    }
+
+    public void setDataDeValidade(String dataDeValidade) {
+        this.dataDeValidade = dataDeValidade;
+    }
 }
