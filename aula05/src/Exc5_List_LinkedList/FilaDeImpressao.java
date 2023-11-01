832a0c3bc13151fb15e63039d6eadfb9139e0f9a
0hTutu <112774071+0htutu@users.noreply.github.com>
2023-11-01 02:14:59 -0300

    Exercicio 5 da aula 05 iniciado! Main, Impressao e FilaDeImpressao criados. Impressão já com os atributos nome e numeroDePaginas e métodos get e set criados. FilaDeImpressao ainda sem nada

---
+++ aula05/src/Exc5_List_LinkedList/FilaDeImpressao.java
@@ -0,0 +1,16 @@
+package src.Exc5_List_LinkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FilaDeImpressao {
+
+    Scanner sc = new Scanner(System.in);
+
+    List<Impressao> FilaDeImpressao = new LinkedList<>();
+
+    public void menu(){
+        System.out.println("Bem vindo á fila de impressão!!");
+    }
+}
---
+++ aula05/src/Exc5_List_LinkedList/Impressao.java
@@ -0,0 +1,23 @@
+package src.Exc5_List_LinkedList;
+
+public class Impressao {
+
+    String nome;
+    int numeroDePaginas;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumeroDePaginas() {
+        return numeroDePaginas;
+    }
+
+    public void setNumeroDePaginas(int numeroDePaginas) {
+        this.numeroDePaginas = numeroDePaginas;
+    }
+}
