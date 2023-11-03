6412a80d5b8a320df0376aecdf5aa0188c0d89e8
0hTutu <112774071+0htutu@users.noreply.github.com>
2023-11-02 21:18:31 -0300

    Exercicio 5 aula05 concluido!

--- aula05/src/Exc5_List_LinkedList/Impressao.java
+++ aula05/src/Exc5_List_LinkedList/Impressao.java
@@ -1,9 +1,19 @@
 package src.Exc5_List_LinkedList;
 
 public class Impressao {
-
+    @Override
+    public String toString() {
+        return "Impressao - " + '\n' +
+                "nome = '" + nome + '\'' +
+                '\n' + " numeroDePaginas = " + numeroDePaginas;
+    }
     String nome;
     int numeroDePaginas;
+
+    public Impressao(String nome, int numeroDePaginas) {
+        this.nome = nome;
+        this.numeroDePaginas = numeroDePaginas;
+    }
 
     public String getNome() {
         return nome;
--- aula05/src/Exc5_List_LinkedList/Main.java
+++ aula05/src/Exc5_List_LinkedList/Main.java
@@ -7,38 +7,52 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        LinkedList<String> imp = new LinkedList<>();
+        LinkedList<Impressao> filaDeImp = new LinkedList<>();
         int op;
         do {
-            System.out.println("-Bem vindo á fila de impressão-");
-            System.out.println("1 - Adcionar documento");
-            System.out.println("2 - Ver documentos");
-            System.out.println("3 - Remover documento");
-            System.out.println("0 - Encerrar programa");
+            System.out.println(""" 
+                   
+                   ==Bem vindo á fila de impressão==
+                        1 - Adcionar documento
+                        2 - Ver documentos
+                    
+                        0 - Encerrar programa
+                    """);
+            System.out.print("-> ");
 
-            sc.nextLine();
+
             op = sc.nextInt();
+            sc.nextLine();
 
             switch (op){
-                case 1:
-                    System.out.println("");
-                    break;
+                case (1) -> {
+                    System.out.print("Insira o nome do documento:");
+                    String nome = sc.nextLine();
+                    System.out.print("Insira o numero de páginas do documento:");
+                    int numeroDePaginas = sc.nextInt();
+                    Impressao imp = new Impressao(nome, numeroDePaginas);
+                    filaDeImp.add(imp);
+                }
 
-                case 2:
-                    System.out.println();
-                    break;
+                case (2) -> {
+                    if(filaDeImp.isEmpty()){
+                        System.out.println("Não há nenhum documento aqui.");
+                    }
+                    else {
+                        for (int i = 0; i < filaDeImp.size(); i++) {
+                            System.out.println(filaDeImp.get(i));
+                        }
+                    }
 
-                case 3:
-                    System.out.println();
-                    break;
 
-                case 0:
-                    break;
+                }
 
-                default:
-                    System.out.println("Esta opção não existe!");
-                    break;
-            }
-        } while (true);
-    }
+                case (0) -> {
+                    return;
+                }
+
+            default -> System.out.println("Esta opção não existe!");
+        }
+    } while (true);
+}
 }
